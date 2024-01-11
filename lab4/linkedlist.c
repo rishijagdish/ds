a9966ba30a01786b205f37493d30623beda6ea8c
rishi <153579789+rishijagdish@users.noreply.github.com>
2024-01-11 13:09:37 +0530

    Create linkedlist.c

---
+++ lab4/linkedlist.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+void insertAtHead(struct node **head_ref, int new_data) {
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void insertAtMiddle(struct node *prev_node, int new_data) {
+    if (prev_node == NULL) {
+        printf("The given previous node cannot be NULL\n");
+        return;
+    }
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = new_data;
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
+}
+
+void insertAtEnd(struct node **head_ref, int new_data) {
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    struct node *last = *head_ref;
+    new_node->data = new_data;
+    new_node->next = NULL;
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+    while (last->next != NULL) {
+        last = last->next;
+    }
+    last->next = new_node;
+    return;
+}
+
+void display(struct node *node) {
+    printf("Linked List:");
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct node *head = NULL;
+    int choice=0,a;
+    while(choice!=3){
+        printf("Select an option\n1.Insert\n2.Display\n3.Exit\nChoice:");
+        scanf("%d",&choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter the value to inserted:");
+            scanf("%d",&a);
+            int choice2;
+            printf("--------------\n1.Insert at Head\n2.Insert at end\n3.Insert in the middle\nChoice:");
+            scanf("%d",&choice2);
+            switch (choice2)
+            {
+            case 1:
+                insertAtHead(&head,a);
+                display(head);
+
+                break;
+            case 2:
+                insertAtMiddle(&head,a);
+                display(head);
+                break;
+            case 3:
+                insertAtEnd(&head,a);
+                display(head);
+                break;
+            }
+            break;
+        case 2:
+            display(head);
+            break;
+        default:
+            break;
+        }
+    }
+
+
+    return 0;
+}
